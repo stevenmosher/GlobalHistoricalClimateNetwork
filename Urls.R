ee339db16fe181e86189d41fc1dd38273bd93184
stevenmosher <moshersteven@gmail.com>
2020-06-10 21:32:13 +0800

    Initial Commit

---
+++ Urls.R
@@ -0,0 +1,7 @@
+############# ftp links for key data
+GHCN_FTP         <- "ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/"
+GHCN_FTP_COUNTRY <- "ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm-countries.txt"
+GHCN_FTP_RAW     <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm.tavg.latest.qcu.tar.gz"
+GHCN_FTP_ADJ     <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm.tavg.latest.qcf.tar.gz"
+GHCN_FTP_FLAGS   <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm-flags.txt"
+GHCN_FTP_README  <- "ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/readme.txt"
