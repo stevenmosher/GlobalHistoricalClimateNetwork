265a82873e1e65adfa07e757621a8d220eecef70
stevenmosher <moshersteven@gmail.com>
2020-06-11 17:02:22 +0800

    filled out skeleton

--- getDir_GHCN.R
+++ getDir_GHCN.R
@@ -1,1 +1,15 @@
-getDir_GHCN<- function()
+getDir_GHCN<- function(directory=getwd()){
+  require(stringr)
+  
+  prefix <-"ghcnm.v4."
+  dirs<- list.dirs(path=directory,full.names=TRUE)
+  ghcndir<-str_detect(dirs,pattern=prefix)
+  if(sum(ghcndir)==0){
+    msg<- paste("No GHCN Folders in ",directory,sep="")
+    stop(msg)
+    
+  }else{
+    dirs<-dirs[ghcndir]
+  }
+  return(dirs)
+}
