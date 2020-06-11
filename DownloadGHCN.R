92d7c79a5342375a3b9cbde9574bc0d2f3dc699f
stevenmosher <moshersteven@gmail.com>
2020-06-11 11:38:43 +0800

    Initial commit

--- DownloadGHCN.R
+++ DownloadGHCN.R
@@ -1,31 +1,12 @@
 source("Libraries.R")
-
-unadj <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm.tavg.latest.qcu.tar.gz"
-adj   <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm.tavg.latest.qcf.tar.gz"
-cc    <-"ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v4/ghcnm-countries.txt"
-Tdir  <- "TemporaryData"
-Cdir  <- "CurrentData"
-bunadj<-file.path(Tdir,basename(unadj))
-badj<-file.path(Tdir,basename( adj))
-
-#download to temp
-#unzip
-#Clean Current
-#Move to Current
-# delete temp
-# call Processing
-if(!dir.exists(Tdir))dir.create(Tdir)
-if(!dir.exists(Cdir))dir.create(Cdir)
-
-filesInTemp <-list.files(Tdir)
-
+source("Urls.R")
  
-  download.file(unadj,destfile=bunadj,mode="wb")
-  gunzip(bunadj,destname=basename(bunadj))
-  untar(basename(bunadj),exdir=Cdir)
-  download.file(adj,destfile=badj,mode="wb")
-  gunzip(badj,destname=basename(badj))
-  untar(basename(badj),exdir=Cdir)
-  
-   
+AdjustedFilename<-file.path(basename(GHCN_FTP_ADJ))
+RawFilename<-file.path(basename(GHCN_FTP_RAW))
+download.file(GHCN_FTP_ADJ,destfile=AdjustedFilename,mode="wb")
+untar(AdjustedFilename)
+download.file(GHCN_FTP_RAW,destfile=RawFilename,mode="wb")
+untar(RawFilename)
+ 
+ 
 
