294f307317761b5d639c4d362b8f232202329cff
stevenmosher <moshersteven@gmail.com>
2020-06-12 12:30:26 +0800

    Changed to the date of the filename

--- filedate_GHCN.R
+++ filedate_GHCN.R
@@ -1,6 +1,7 @@
-filedate_GHCN<- function(directory=NA){
+filedate_GHCN<- function(filename=NA){
   require(stringr)
-  filedate <- str_sub(directory,nchar(directory)-7,nchar(directory))
+  filename<-str_sub(filename,1,nchar(filename)-8)
+  filedate <- str_sub(filename,nchar(filename)-7,nchar(filename))
   return(as.Date(filedate,format="%Y%m%d"))
    
 }
