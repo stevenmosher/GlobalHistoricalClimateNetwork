3c1822dd03478f8e021518eab155b5eb8adf1aa3
stevenmosher <moshersteven@gmail.com>
2020-06-12 12:32:23 +0800

    initial commit

---
+++ getInv_GHCN.R
@@ -0,0 +1,26 @@
+getInv_GHCN<- function(directory=getwd(),Adjusted=TRUE){
+  require(stringr)
+  
+  ext <- ".inv$"
+  adj <- "qcf"
+  raw <- "qcu"
+  D<- list.files(path=directory,full.names=TRUE,recursive=TRUE, pattern=ext)
+  if(Adjusted){
+    D<-D[str_detect(D,pattern=adj)]
+    
+  }else{
+    D<-D[str_detect(D,pattern=raw)]
+  }
+  
+   
+   
+  if(length(D)==0){
+    msg<- paste("No GHCN Inventories in ",directory,sep="")
+    stop(msg)
+    
+  } 
+  D<-tbl_df(data.frame(Filename=D,stringsAsFactors = F))
+  D<-D %>% mutate(Date=filedate_GHCN(Filename)) %>% arrange(desc(Date))
+  
+  return(D)
+}
