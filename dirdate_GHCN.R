6d8a2b54f3360cf35c2979473f5b5f5a4c9f223e
stevenmosher <moshersteven@gmail.com>
2020-06-12 12:33:39 +0800

    initial commit

---
+++ dirdate_GHCN.R
@@ -0,0 +1,7 @@
+dirdate_GHCN<- function(directory=NA){
+  require(stringr)
+   
+  filedate <- str_sub(directory,nchar(directory)-7,nchar(directory))
+  return(as.Date(filedate,format="%Y%m%d"))
+   
+}
